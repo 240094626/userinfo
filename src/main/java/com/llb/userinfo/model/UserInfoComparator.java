f2f9e081d95108c9c493741c4f4583550881104b
240094626 <michael-ketty@qq.com>
2018-03-23 15:02:10 +0800

    comparator name toupper

--- src/main/java/com/llb/userinfo/model/UserInfoComparator.java
+++ src/main/java/com/llb/userinfo/model/UserInfoComparator.java
@@ -2,6 +2,8 @@ package com.llb.userinfo.model;
 
 import java.util.Comparator;
 
+import org.springframework.util.StringUtils;
+
 /**
  * 用户信息比较器
  * @author linlb
@@ -17,25 +19,29 @@ public class UserInfoComparator implements Comparator<UserInfo> {
 	/**降序*/
 	public static final String TYPE_DESC ="1";
 	
-	private String name;
+	private String name = "ID";
 	
 	private String type = "0";
 
 	public UserInfoComparator(String name,String type) {
-		this.name = name;
-		this.type = type;
+		if(!StringUtils.isEmpty(name)){
+			this.name = name.toUpperCase();
+		}
+		if(!StringUtils.isEmpty(name)){
+			this.type = type;
+		}
 	}
 	
 	@Override
 	public int compare(UserInfo u1, UserInfo u2) {
-		if(NAME_ID.equals(name) ){
+		if(NAME_ID.equals(name.toUpperCase()) ){
 			// 按ID排序，默认升序
 			if(TYPE_ASC.equals(type)){
 				return Long.valueOf(u1.getId()).compareTo(u2.getId());
 			}else{
 				return Long.valueOf(u2.getId()).compareTo(u1.getId());
 			}
-		}else if(NAME_LOGINNAME.equals(name)){
+		}else if(NAME_LOGINNAME.equals(name.toUpperCase())){
 			// 按登录名排序，默认升序
 			if(TYPE_ASC.equals(type)){
 				return u1.getLoginname().compareTo(u2.getLoginname());
@@ -43,7 +49,6 @@ public class UserInfoComparator implements Comparator<UserInfo> {
 				return u2.getLoginname().compareTo(u1.getLoginname());
 			}
 		}
-		// 默认按ID排序，默认升序
 		return Long.valueOf(u1.getId()).compareTo(u2.getId());
 	}
 
